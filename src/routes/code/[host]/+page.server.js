ebba96afd3741b8f91e6f1da646cb2328406ea4e
V <78769380+codegod100@users.noreply.github.com>
2023-08-12 05:11:51 +0000

    prototyping

---
+++ src/routes/code/[host]/+page.server.js
@@ -0,0 +1,67 @@
+import { createClient } from '@supabase/supabase-js'
+import { PROJECT_URL, API_KEY, REDIRECT_URI } from '$env/static/private';
+import { redirect } from '@sveltejs/kit';
+const supabase = createClient(PROJECT_URL, API_KEY)
+
+async function getApp(host) {
+    return await supabase
+        .from('applications')
+        .select()
+        .order("created_at", { ascending: false })
+        .eq("host", host)
+        .limit(1)
+        .single()
+}
+
+export async function load({ url, fetch, request, params }) {
+    const code = url.searchParams.get('code')
+    const host = params.host
+    let resp = await getApp(host)
+    console.log(host, resp)
+    const body = new FormData()
+    body.append("client_id", resp.data.client_id)
+    body.append("client_secret", resp.data.client_secret)
+    body.append("redirect_uri", `${REDIRECT_URI}/${host}`)
+    body.append("grant_type", "authorization_code")
+    body.append("code", code)
+    body.append("scope", "read write push")
+
+    const res = await fetch(`https://${host}/oauth/token`, {
+        method: "POST",
+        body
+    })
+    const token = await res.json()
+    console.log(token)
+
+    let res2 = await fetch(`https://${host}/api/v1/accounts/verify_credentials`, {
+        headers: { 'Authorization': `Bearer ${token.access_token}` }
+    })
+    const account = await res2.json()
+    console.log(account)
+    if (account.username) {
+        const username = account.username
+        const access_token = token.access_token
+
+
+        let resp = await supabase
+            .from('users')
+            .select()
+            .order("created_at", { ascending: false })
+            .eq("username", username)
+            .eq("host", host)
+            .limit(1)
+            .single()
+
+        console.log(resp)
+        if (!resp.data) {
+
+            let resp2 = await supabase
+                .from('users')
+                .insert({ username, access_token, host })
+            console.log(resp2)
+        }
+    }
+    return {
+        code
+    };
+}
---
+++ src/routes/create/[host]/+page.server.js
@@ -0,0 +1,47 @@
+import { createClient } from '@supabase/supabase-js'
+import { PROJECT_URL, API_KEY, REDIRECT_URI } from '$env/static/private';
+import { redirect } from '@sveltejs/kit';
+const supabase = createClient(PROJECT_URL, API_KEY)
+
+async function getApp(host){
+    return await supabase
+    .from('applications')
+    .select()
+    .order("created_at", { ascending: false })
+    .eq("host", host)
+    .limit(1)
+    .single()
+}
+
+export async function load({ fetch, params }) {
+    const host = params.host
+    let resp = await getApp(host)
+
+
+    console.log(resp.data)
+
+
+    if(resp.data && resp.data.client_id){
+        throw redirect(307,`https://${host}/oauth/authorize?client_id=${resp.data.client_id}&scope=read+write+push&redirect_uri=${REDIRECT_URI}/${host}&response_type=code&host=${host}`)
+        return {item: resp.data}
+    }
+
+    const body = new FormData()
+    body.append("client_name", "tootbooster")
+    body.append("redirect_uris", `${REDIRECT_URI}/${host}`)
+    body.append("scopes", "read write push")
+    const url = `https://${host}/api/v1/apps`
+    const res = await fetch(url, {
+        method: 'POST',
+        body
+    }
+    );
+    console.log("trying")
+    const item = await res.json();
+    console.log(item)
+    let resp2 = await supabase
+    .from('applications')
+    .insert({client_id: item.client_id, client_secret: item.client_secret, host})
+    console.log(resp2.error, resp2.statusText)
+    return { item };
+}
