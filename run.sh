f6e0df189af9c8229b5afd127753f8feb86418c0
Veronika Gillies <veronikamwinters@gmail.com>
2023-08-23 05:18:06 +0000

    will this work without trust?

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $DBHOST
 ping -c 1 $DBHOST
-~/.local/bin/edgedb instance link --dsn edgedb://edgedb:$DBPASSWD@$DBHOST
+~/.local/bin/edgedb instance link --dsn edgedb://edgedb:$DBPASSWD@$DBHOST --non-interactive
 npm run build
 bun build/index.js
